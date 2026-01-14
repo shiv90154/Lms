127122095982ed94491231c04fce02b67804199e
shiv_kumar <shiva90154@gmail.com>
2026-01-15 03:49:24 +0530

    final all project done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   theme: {
     extend: {
       colors: {
@@ -69,4 +73,4 @@ const config = {
   plugins: [],
 }
 
-export default config
+export default config;
